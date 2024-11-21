5c2b4e7d265671bd2bc86d97547294da35d356fb
VeroVelas <veronicavelasco2778@gmail.com>
2024-11-21 14:51:37 -0600

    proxi

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,45 @@
 // src/index.ts
 import express from "express";
-import healthRoutes from "./infrastructure/routes/healthRoutes";
-import userRoutes from "./infrastructure/routes/userRoutes";
-import orderRoutes from "./infrastructure/routes/OrderRouter"; // Corrección aquí
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 const app = express();
-const PORT = 3002;
+const PORT = 3000;
 
 app.use(express.json());
 
-// Configuración de rutas
-app.use("/api", healthRoutes);
-app.use("/usuarios", userRoutes);
-app.use("/pedidos", orderRoutes);
+// Health Check del Gateway
+app.get("/health", (req, res) => {
+  res.status(200).send("API Gateway is healthy!");
+});
 
-app.listen(3002, () => {
-  console.log("API corriendo en el puerto 3002");
+// Proxy para el servicio de usuarios
+app.use(
+  "/usuarios",
+  createProxyMiddleware({
+    target: "http://localhost:3001", // Dirección del servicio de usuarios
+    changeOrigin: true,
+  })
+);
+
+// Proxy para el servicio de productos
+app.use(
+  "/productos",
+  createProxyMiddleware({
+    target: "http://localhost:3002", // Dirección del servicio de productos
+    changeOrigin: true,
+  })
+);
+
+// Proxy para el servicio de pedidos
+app.use(
+  "/pedidos",
+  createProxyMiddleware({
+    target: "http://localhost:3003", // Dirección del servicio de pedidos
+    changeOrigin: true,
+  })
+);
+
+// Iniciar el servidor en el puerto especificado
+app.listen(PORT, () => {
+  console.log(`API Gateway corriendo en http://localhost:${PORT}`);
 });
